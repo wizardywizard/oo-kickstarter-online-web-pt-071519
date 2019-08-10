8a12410d14f169036278e830ca9fa371af78a3e9
David Little <littledavid175@gmail.com>
2019-08-10 16:37:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,9 +2,10 @@ class Backer
   
  attr_reader :backed_projects, :name 
   
-  def initialize(name)
+  def initialize(name, project)
     @backed_projects = []
     @name = name
+    @backed_projects << project
   end
   
   
